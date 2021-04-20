e98c40fac2c7f920cdc582b886024ce61f6e3ea4
Bae0903 <akmal.abesahc33@gmail.com>
2021-04-20 12:01:19 +0800

    Add project files.

---
+++ Function and Sub/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSubmit1 = New System.Windows.Forms.Button()
+        Me.lblText1 = New System.Windows.Forms.Label()
+        Me.lblText2 = New System.Windows.Forms.Label()
+        Me.lblText3 = New System.Windows.Forms.Label()
+        Me.lblText4 = New System.Windows.Forms.Label()
+        Me.btnSubmit2 = New System.Windows.Forms.Button()
+        Me.btnSubmit3 = New System.Windows.Forms.Button()
+        Me.btnSubmit4 = New System.Windows.Forms.Button()
+        Me.txtBox1 = New System.Windows.Forms.TextBox()
+        Me.txtBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnSubmit1
+        '
+        Me.btnSubmit1.AccessibleName = "btnSubmit1"
+        Me.btnSubmit1.Location = New System.Drawing.Point(329, 90)
+        Me.btnSubmit1.Name = "btnSubmit1"
+        Me.btnSubmit1.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit1.TabIndex = 0
+        Me.btnSubmit1.Text = "Submit"
+        Me.btnSubmit1.UseVisualStyleBackColor = True
+        '
+        'lblText1
+        '
+        Me.lblText1.AccessibleName = "lblText1"
+        Me.lblText1.AutoSize = True
+        Me.lblText1.Location = New System.Drawing.Point(422, 95)
+        Me.lblText1.Name = "lblText1"
+        Me.lblText1.Size = New System.Drawing.Size(39, 13)
+        Me.lblText1.TabIndex = 1
+        Me.lblText1.Text = "Label1"
+        '
+        'lblText2
+        '
+        Me.lblText2.AccessibleName = "lblText2"
+        Me.lblText2.AutoSize = True
+        Me.lblText2.Location = New System.Drawing.Point(422, 124)
+        Me.lblText2.Name = "lblText2"
+        Me.lblText2.Size = New System.Drawing.Size(39, 13)
+        Me.lblText2.TabIndex = 2
+        Me.lblText2.Text = "Label2"
+        '
+        'lblText3
+        '
+        Me.lblText3.AccessibleName = "lblText3"
+        Me.lblText3.AutoSize = True
+        Me.lblText3.Location = New System.Drawing.Point(422, 153)
+        Me.lblText3.Name = "lblText3"
+        Me.lblText3.Size = New System.Drawing.Size(39, 13)
+        Me.lblText3.TabIndex = 3
+        Me.lblText3.Text = "Label3"
+        '
+        'lblText4
+        '
+        Me.lblText4.AccessibleName = "lblText4"
+        Me.lblText4.AutoSize = True
+        Me.lblText4.Location = New System.Drawing.Point(422, 182)
+        Me.lblText4.Name = "lblText4"
+        Me.lblText4.Size = New System.Drawing.Size(39, 13)
+        Me.lblText4.TabIndex = 4
+        Me.lblText4.Text = "Label4"
+        '
+        'btnSubmit2
+        '
+        Me.btnSubmit2.AccessibleName = "btnSubmit2"
+        Me.btnSubmit2.Location = New System.Drawing.Point(329, 119)
+        Me.btnSubmit2.Name = "btnSubmit2"
+        Me.btnSubmit2.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit2.TabIndex = 5
+        Me.btnSubmit2.Text = "Submit"
+        Me.btnSubmit2.UseVisualStyleBackColor = True
+        '
+        'btnSubmit3
+        '
+        Me.btnSubmit3.AccessibleName = "btnSubmit3"
+        Me.btnSubmit3.Location = New System.Drawing.Point(329, 148)
+        Me.btnSubmit3.Name = "btnSubmit3"
+        Me.btnSubmit3.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit3.TabIndex = 6
+        Me.btnSubmit3.Text = "Submit"
+        Me.btnSubmit3.UseVisualStyleBackColor = True
+        '
+        'btnSubmit4
+        '
+        Me.btnSubmit4.AccessibleName = "btnSubmit4"
+        Me.btnSubmit4.Location = New System.Drawing.Point(329, 177)
+        Me.btnSubmit4.Name = "btnSubmit4"
+        Me.btnSubmit4.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit4.TabIndex = 7
+        Me.btnSubmit4.Text = "Submit"
+        Me.btnSubmit4.UseVisualStyleBackColor = True
+        '
+        'txtBox1
+        '
+        Me.txtBox1.AccessibleName = "txtBox1"
+        Me.txtBox1.Location = New System.Drawing.Point(214, 121)
+        Me.txtBox1.Name = "txtBox1"
+        Me.txtBox1.Size = New System.Drawing.Size(100, 20)
+        Me.txtBox1.TabIndex = 8
+        '
+        'txtBox2
+        '
+        Me.txtBox2.AccessibleName = "txtBox2"
+        Me.txtBox2.Location = New System.Drawing.Point(214, 179)
+        Me.txtBox2.Name = "txtBox2"
+        Me.txtBox2.Size = New System.Drawing.Size(100, 20)
+        Me.txtBox2.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtBox2)
+        Me.Controls.Add(Me.txtBox1)
+        Me.Controls.Add(Me.btnSubmit4)
+        Me.Controls.Add(Me.btnSubmit3)
+        Me.Controls.Add(Me.btnSubmit2)
+        Me.Controls.Add(Me.lblText4)
+        Me.Controls.Add(Me.lblText3)
+        Me.Controls.Add(Me.lblText2)
+        Me.Controls.Add(Me.lblText1)
+        Me.Controls.Add(Me.btnSubmit1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSubmit1 As Button
+    Friend WithEvents lblText1 As Label
+    Friend WithEvents lblText2 As Label
+    Friend WithEvents lblText3 As Label
+    Friend WithEvents lblText4 As Label
+    Friend WithEvents btnSubmit2 As Button
+    Friend WithEvents btnSubmit3 As Button
+    Friend WithEvents btnSubmit4 As Button
+    Friend WithEvents txtBox1 As TextBox
+    Friend WithEvents txtBox2 As TextBox
+End Class
