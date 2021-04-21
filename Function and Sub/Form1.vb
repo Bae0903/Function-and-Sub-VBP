cd047cc2ce7233472295e9fe62a98da617470cc8
Bae0903 <akmal.abesahc33@gmail.com>
2021-04-21 11:56:55 +0800

    Commit

--- Function and Sub/Form1.vb
+++ Function and Sub/Form1.vb
@@ -3,6 +3,7 @@
     Private Sub S1()
         lblText1.Text = "Hello"
     End Sub
+
     Private Sub btnSubmit1_Click(sender As Object, e As EventArgs) Handles btnSubmit1.Click
         S1()
     End Sub
@@ -10,6 +11,7 @@
     Private Sub S2(ByVal strText As String)
         lblText2.Text = strText
     End Sub
+
     Private Sub btnSubmit2_Click(sender As Object, e As EventArgs) Handles btnSubmit2.Click
         S2(txtBox1.Text)
     End Sub
@@ -17,6 +19,7 @@
     Private Function S3()
         Return "Hello"
     End Function
+
     Private Sub btnSubmit3_Click(sender As Object, e As EventArgs) Handles btnSubmit3.Click
         lblText3.Text = S3()
     End Sub
@@ -24,6 +27,7 @@
     Private Function S4(ByVal strText2 As String)
         Return strText2
     End Function
+
     Private Sub btnSubmit4_Click(sender As Object, e As EventArgs) Handles btnSubmit4.Click
         lblText4.Text = S4(txtBox2.Text)
     End Sub
